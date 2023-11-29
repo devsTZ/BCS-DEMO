7c72e32de812811bc9fbad262ffce0eaa7b6b1fd
devsTz <rogashianmvungi@gmail.com>
2023-11-29 10:26:31 +0400

    date, include and require function

---
+++ php-function/date_time.php
@@ -0,0 +1,17 @@
+<?php
+// date() function return the formated string representing date
+
+//current date i.e .29-11-2023
+echo date("d-m-Y") . "<br>";
+
+// current time 12hr
+echo date('h:i:s:a');
+// current time 24hr
+echo "<br>". date('H:i:s:a');
+
+echo '<br>month ' . date('M');
+
+
+echo '<br>' . date('Y-m-d');
+
+?>
---
+++ php-function/include&require.php
@@ -0,0 +1,9 @@
+<?php
+require './date_time.php';
+include './date_time.php';
+
+// only display once
+include_once "./func.php";
+require_once './date_time.php'
+
+?>
